bf0bb3dbead6ac1fd7277671509dd1594579960c
Matt Derocher <mjamesderocher@gmail.com>
2023-01-02 14:37:34 -0500

    Started basic site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="section">
+
+  <img src="/logo.svg" alt="The logo for Laura's House" />
+
+  <h1>Welcome to Laura's House</h1>
+
+  <p>Laura's House is a non-profit organization that raises funds to help orphans, single-parent families, people with disabilities, and other people with needs in Singida, Tanzania, Africa.</p>
+
+  <p>This is a new site and we don't have much up now, but we will try to have more up soon.</p>
+
+  <h2>Donate</h2>
+  <div class="buttons">
+    <a href="https://paypal.me/laurashouse1"><img alt="PayPal logo" src="/paypal-logo.svg" /></a>
+  </div>
+
+  <h2>Contact Information</h2>
+  <p>email: hello@lauras.house</p>
+  <p>address: 53 County Rd, Scarborough, ME 04074</p>
+
+</div>
+
+<style>
+  img {
+    max-width: 200px;
+  }
+
+  .section {
+    max-width: 800px;
+    margin: 0 auto;
+  }
+</style>
